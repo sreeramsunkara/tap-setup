716d4223349583aac3ad432887d409d8c9e0b455
sreeramsunkara <sr_sunkara@yahoo.com>
2023-05-24 15:18:35 -0400

    updated scan-policy for grype

--- metadata-setup-steps.sh
+++ metadata-setup-steps.sh
@@ -1,8 +1,8 @@
-nslookup metadata-store.tap.tanzu.y-compiles.com
-curl https://metadata-store.tap.tanzu.y-compiles.com/api/health -k -v
+nslookup metadata-store.tap.tanzu4u.net
+curl https://metadata-store.tap.tanzu4u.net/api/health -k -v
 
-kubectl get secret ingress-cert -n metadata-store -o json | jq -r '.data."ca.crt"' | base64 -d > insight-ca.crt
-tanzu insight config set-target https://metadata-store.tap.tanzu.y-compiles.com --ca-cert insight-ca.crt
+kubectl get secret tls -n tanzu-system-ingress -o json | jq -r '.data."tls.crt"' | base64 -d > insight-ca.crt
+tanzu insight config set-target https://metadata-store.tap.tanzu4u.net --ca-cert insight-ca.crt
 tanzu insight health
 #To retrieve the read-only access token, run the following command:
 export METADATA_STORE_ACCESS_TOKEN=$(kubectl get secrets metadata-store-read-write-client -n metadata-store -o jsonpath="{.data.token}" | base64 -d)
